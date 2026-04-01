adc3121f81cf62ed53400a85c0434ef406c6539d
Sergio726 <sergio.sebass03@gmail.com>
2026-04-01 20:10:39 -0300

    refactor(vite.config): remove VIP landing page entry from Vite configuration

--- more-landing/vite.config.ts
+++ more-landing/vite.config.ts
@@ -16,7 +16,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html'),
-        vip: path.resolve(__dirname, 'vip.html'),
       },
     },
   },
